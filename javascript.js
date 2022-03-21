09213c6dbacc6835938883ea3a03b21374d0c187
Aryan <40327539+ArynRama@users.noreply.github.com>
2022-03-21 20:41:11 -0300

    honk

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-var navLinks = document.getElementById("navlink");
-
 function showMenu() {
-    navLinks.style.left = "0px";
+    document.getElementById("sideNav").style.width = "70%";
+    document.getElementById("content").style.marginLeft = "70%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 function hideMenu() {
-    navLinks.style.left = "-250px";
+    document.getElementById("sideNav").style.width = "0%";
 }
