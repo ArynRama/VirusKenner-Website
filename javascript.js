710e4a9c7a8d074bbdfd33ed84ec5cf288e4c7f2
Aryan <40327539+ArynRama@users.noreply.github.com>
2022-03-21 22:44:10 -0300

    get

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 function showMenu() {
     document.getElementById("sideNav").style.left = "0px";
-    document.getElementById("content").style.marginLeft = "70%";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 function hideMenu() {
     document.getElementById("sideNav").style.left = "-300px";
