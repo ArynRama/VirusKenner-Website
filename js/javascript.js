dbaed0aa5aec97448e5745bd84e405c25a3367f0
Aryan <40327539+ArynRama@users.noreply.github.com>
2022-03-22 18:29:01 -0300

    bo

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,8 @@
+if (window.innerWidth < 700) {
+    document.getElementById("background").setAttribute("src", "./img/background_mobile.png");
+} else {
+    document.getElementById("background").setAttribute("src", "./img/background.img");
+}
 function showMenu() {
     document.getElementById("sideNav").style.left = "0px";
 }
