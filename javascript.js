d1adf0964d4cceb79f73630686e5fc652cc18973
Aryan <40327539+ArynRama@users.noreply.github.com>
2022-03-21 21:01:32 -0300

    bo

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 function showMenu() {
-    document.getElementById("sideNav").style.width = "70%";
+    document.getElementById("sideNav").style.left = "0px";
     document.getElementById("content").style.marginLeft = "70%";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 function hideMenu() {
-    document.getElementById("sideNav").style.width = "0%";
+    document.getElementById("sideNav").style.left = "-300px";
 }
