52cc3373c06ae4f4010965b7035138f66dd1f0ac
Aryan <40327539+ArynRama@users.noreply.github.com>
2022-03-25 14:40:55 -0300

    nk

--- fact-or-cap/fact-or-cap.js
+++ fact-or-cap/fact-or-cap.js
@@ -1,23 +1,34 @@
 var timer;
-var timeLeft = 15; // seconds
-
+const timeLeft = 15; // seconds
+var score_tot = 1;
+var score_corect = 0;
 // What to do when the timer runs out
 function gameOver() {
     // This cancels the setInterval, so the updateTimer stops getting called
     $('#playAgainButton').show();
     $('#cloud').hide();
     $('#timer').hide();
-    clearInterval(timer)
-
+    clearInterval(timer);
+    if (score_corect / score_tot >= 0.5) {
+        $('#end_message2').html("You know quite a bit about HIV.");
+        $('#end_message3').html("Keep it up.");
+        document.getElementById("end_messages").style.color = "lime";
+    } else {
+        $('#end_message2').html("You're still learing about HIV.");
+        $('#end_message3').html("Keep Learning.");
+        document.getElementById("end_messages").style.color = "red";
+    }
+    $('#end_message1').html(`You have ${score_corect} correct out of ${score_tot}.`);
+    $('#end_messages').show();
     // re-show the button, so they can start it again
 }
 
 function updateTimer() {
-    timeLeft = timeLeft - 1;
-    if (timeLeft >= 10){
-        $('#time').html(timeLeft);
-    } else if (timeLeft>=0){
-        $('#time').html("0" + timeLeft);
+    time = time - 1;
+    if (time >= 10){
+        $('#time').html(time);
+    } else if (time>=0){
+        $('#time').html("0" + time);
     } else {
         gameOver();
     }
@@ -27,6 +38,24 @@ function updateTimer() {
 function start() {
     // setInterval is a built-in function that will call the given function
     // every N milliseconds (1 second = 1000 ms)
+    time = timeLeft
+    timer = setInterval(updateTimer, 1000);
+
+    // It will be a whole second before the time changes, so we'll call the update
+    // once ourselves
+    updateTimer();
+
+    // We don't want the to be able to restart the timer while it is running,
+    // so hide the button.
+    $('#playButton').hide();
+    $('#timer').show();
+    $('#explination').hide();
+    $('#cloud').show();
+}
+function restart() {
+    // setInterval is a built-in function that will call the given function
+    // every N milliseconds (1 second = 1000 ms)
+    time = timeLeft
     timer = setInterval(updateTimer, 1000);
 
     // It will be a whole second before the time changes, so we'll call the update
@@ -37,6 +66,14 @@ function start() {
     // so hide the button.
     $('#playAgainButton').hide();
     $('#timer').show();
-    $('#explination').hide();
+    $('#end_messages').hide();
     $('#cloud').show();
+}
+
+function score() {
+    score_tot++;
+}
+function score_cor() {
+    score_corect++;
+    score_tot++;
 }
