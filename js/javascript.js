d3392bcf91911c39d47627e4b2ec6cee9d720e22
Aryan <40327539+ArynRama@users.noreply.github.com>
2022-03-25 13:39:22 -0300

    mk

--- js/javascript.js
+++ js/javascript.js
@@ -5,16 +5,18 @@ function hideMenu() {
     document.getElementById("sideNav").style.left = "-300px";
 }
 function onLoad() {
-    var height = screen.height
-    var width = screen.width
-    var content = document.getElementsByClassName("content").item(0)
-    content.style.height = `${height - 58}px`
-    content.style.width = `${width}px`
-    content.style.maxWidth = `${width}px`
-    if (visualViewport > 700) {
-        document.getElementById("background").setAttribute("src", "../img/background.png");
-    } else {
-        document.getElementById("background").setAttribute("src", "../img/background_mobile.png");
-    }
+    $(document).ready(function () {
+        var height = jQuery(window).height()
+        var width = jQuery(window).width()
+        var content = document.getElementsByClassName("content").item(0)
+        content.style.height = `${height - 58}px`
+        content.style.width = `${width}px`
+        content.style.maxWidth = `${width}px`
+        if (jQuery(window).width() > 700) {
+            document.getElementById("background").setAttribute("src", "../img/background.png");
+        } else {
+            document.getElementById("background").setAttribute("src", "../img/background_mobile.png");
+        }
+    });
 }
 window.onresize = onLoad()
