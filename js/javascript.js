08bd5a78b845e45c9335343c96736f77ae8e6740
Aryan <40327539+ArynRama@users.noreply.github.com>
2022-03-26 18:36:58 -0300

    n

--- js/javascript.js
+++ js/javascript.js
@@ -19,4 +19,51 @@ function onLoad() {
         }
     });
 }
+var pos = 1
+function interv_left() {
+    var video1 = document.getElementById("video1");
+    var video2 = document.getElementById("video2");
+    var video3 = document.getElementById("video3");
+    var video4 = document.getElementById("video4");
+    var cl = document.getElementById("left_C");
+    var cr = document.getElementById("right_C");
+    pos--
+    if (pos == 1) {
+        video2.style.left = "1500px";
+        video1.style.left = "0";
+        cl.setAttribute("onclick", "");
+        cl.style.color = "#5f5f5f";
+    } else if (pos == 2) {
+        video3.style.left = "1500px";
+        video2.style.left = "0";
+    } else if (pos == 3) {
+        video4.style.left = "1500px";
+        video3.style.left = "0";
+        cr.setAttribute("onclick", "invterv_right()");
+        cr.style.color = "#000000";
+    };
+}
+function interv_right() {
+    var video1 = document.getElementById("video1");
+    var video2 = document.getElementById("video2");
+    var video3 = document.getElementById("video3");
+    var video4 = document.getElementById("video4");
+    var cl = document.getElementById("left_C");
+    var cr = document.getElementById("right_C");
+    pos++
+    if (pos == 4) {
+        video3.style.left = "-500px";
+        video4.style.left = "0";
+        cr.setAttribute("onclick", "");
+        cr.style.color = "#5f5f5f";
+    } else if (pos == 3) {
+        video2.style.left = "-500px";
+        video3.style.left = "0";
+    } else if (pos == 2) {
+        video2.style.left = "-500px";
+        video1.style.left = "0";
+        cl.setAttribute("onclick", "invterv_left()");
+        cl.style.color = "#000000";
+    }
+}
 window.onresize = onLoad()
--- todo.md
+++ todo.md
@@ -5,16 +5,16 @@ Essential{
     statistics doen
 
     Home screen maken
-        -fotos van ons
-        -behind the scenes
-        -introduction over ons geven
+        ~-fotos van ons~
+        ~-behind the scenes~
+        ~-introduction over ons geven~
     discussion veranderen naar link met stichting lobby
     
     
 }
 Problems{
     ~Header mobile fixen~
-    die fotos van ons zijn beetje broko op mobile
+    die fotos van ons zijn beetje broko op mobile(**Ga ik nu niet doen want we hebben nu alleen die pc version nodig**)
     die fotos zijn in text
     die afrika hoort bij history en niet bij statistics
     die general info is nu helemaal tot einde van screen weer en interviews ook
@@ -27,5 +27,6 @@ design{
     text van home page reorderen/more photos
     numbers even groot maken
     pics bij HIV goed zetten
+    homescreen beter designen
 }
     
